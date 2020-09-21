df3c18cf28ed4465f60b4f612c3128070d016e34
AdityaManikanth2810 <adityamanikanthrao@gmail.com>
2020-09-21 23:46:45 +0530

    completed Assignment 2 in Chap 5: Working with Express.js

--- app.js
+++ app.js
@@ -1,5 +1,22 @@
-const http = require('http');
-const routes = require('./routes');
+const express = require('express');
 
-const server = http.createServer(routes);
-server.listen(3000);
+const app = express();
+
+app.use('/users', (req, res, next) => {
+    res.send('<h1>Here is a list of Users</h1>');
+});
+
+app.use('/', (req, res, next) => {
+    res.send('<h1>Hello Default Route</h1>');
+});
+
+// app.use((req, res, next) => {
+//     console.log('In the 1st middleware');
+//     next();
+// });
+
+// app.use((req, res, next) => {
+//     console.log('In the 2nd middleware');
+// });
+
+app.listen(3000);
