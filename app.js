d1112e2f1fc900058e53c88a004010da15d9158a
AdityaManikanth2810 <adityamanikanthrao@gmail.com>
2020-09-22 19:18:16 +0530

    end of chapter 5 : Working with Express.js

--- app.js
+++ app.js
@@ -1,22 +1,20 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
 const app = express();
 
-app.use('/users', (req, res, next) => {
-    res.send('<h1>Here is a list of Users</h1>');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(__dirname, "views", "page-not-found.html"));
 });
-
-app.use('/', (req, res, next) => {
-    res.send('<h1>Hello Default Route</h1>');
-});
-
-// app.use((req, res, next) => {
-//     console.log('In the 1st middleware');
-//     next();
-// });
-
-// app.use((req, res, next) => {
-//     console.log('In the 2nd middleware');
-// });
 
 app.listen(3000);
