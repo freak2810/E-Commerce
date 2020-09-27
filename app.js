3ce969074f99d908c42db235722303556c323c48
AdityaManikanth2810 <adityamanikanthrao@gmail.com>
2020-09-27 11:10:04 +0530

    cleaned up the project

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
-const express = require('express');
-const bodyParser = require('body-parser');
 const path = require('path');
 
-const adminData = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
+const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
+
+const adminData = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -17,7 +18,7 @@ app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).render('404', { pageTitle: 'Page Not Found' });
+  res.status(404).render('404', { pageTitle: 'Page Not Found' });
 });
 
 app.listen(3000);
