12324152b94d51d5952ff2ac516a993442d558c0
AdityaManikanth2810 <adityamanikanthrao@gmail.com>
2020-09-29 09:08:58 +0530

    completed chapter 7 : MVC

---
+++ models/product.js
@@ -0,0 +1,40 @@
+const { create } = require('domain');
+const fs = require('fs');
+const path = require('path');
+
+const p = path.join(
+    path.dirname(process.mainModule.filename),
+    'data',
+    'products.json'
+);
+
+
+const getProductsFromFile = cb => {
+    fs.readFile(p, (err, fileContent) => {
+        if (err) {
+            cb([]);
+        } else {
+            cb(JSON.parse(fileContent));
+        }
+    });
+};
+
+module.exports = class Product {
+    constructor(title) {
+        this.title = title;
+    }
+
+    save() {
+        getProductsFromFile(products => {
+            products.push(this);
+            fs.writeFile(p, JSON.stringify(products), (err) => {
+                console.log(err);
+            });
+        });
+    }
+
+    //To call the function directly without instantiating an object
+    static fetchAll(cb) {
+        getProductsFromFile(cb);
+    }
+};
