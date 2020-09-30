5ec23379046eca167254b11e3aca361a1e85afcd
AdityaManikanth2810 <adityamanikanthrao@gmail.com>
2020-09-30 16:31:02 +0530

    End of Chapter 8 : Enhancing The App

--- routes/shop.js
+++ routes/shop.js
@@ -1,8 +1,19 @@
+const path = require('path');
+
 const express = require('express');
-const { getProducts } = require('../controllers/products');
+
+const shopController = require('../controllers/shop');
 
 const router = express.Router();
 
-router.get('/', getProducts);
+router.get('/', shopController.getIndex);
+
+router.get('/products', shopController.getProducts);
+
+router.get('/cart', shopController.getCart);
+
+router.get('/orders', shopController.getOrders);
+
+router.get('/checkout', shopController.getCheckout);
 
 module.exports = router;
