70d5a1aa73e746f4232a59c065fb2146441e5123
AdityaManikanth2810 <adityamanikanthrao@gmail.com>
2020-11-05 17:56:05 +0530

    completed chap 15 - Authentication

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const session = require('express-session');
 const MongoDBStore = require('connect-mongodb-session')(session);
+const csrf = require('csurf');
+const flash = require('connect-flash');
 
 const errorController = require('./controllers/error');
 const User = require('./models/user');
@@ -14,8 +16,9 @@ const MONGODB_URI = 'mongodb+srv://freak2810:Sheena&mani01@node-course.jvuac.mon
 const app = express();
 const store = new MongoDBStore({
   uri: MONGODB_URI,
-  collection: 'sessions',
+  collection: 'sessions'
 });
+const csrfProtection = csrf();
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -26,17 +29,16 @@ const authRoutes = require('./routes/auth');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
-
 app.use(
-  session(
-    {
-      secret: 'mysecret',
-      resave: false,
-      saveUninitialized: false,
-      store: store
-    }
-  )
+  session({
+    secret: 'my secret',
+    resave: false,
+    saveUninitialized: false,
+    store: store
+  })
 );
+app.use(csrfProtection);
+app.use(flash());
 
 app.use((req, res, next) => {
   if (!req.session.user) {
@@ -48,7 +50,12 @@ app.use((req, res, next) => {
       next();
     })
     .catch(err => console.log(err));
+});
 
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.session.isLoggedIn;
+  res.locals.csrfToken = req.csrfToken();
+  next();
 });
 
 app.use('/admin', adminRoutes);
@@ -63,21 +70,8 @@ mongoose
     {
       useNewUrlParser: true,
       useUnifiedTopology: true
-    }
-  )
+    })
   .then(result => {
-    User.findOne().then(user => {
-      if (!user) {
-        const user = new User({
-          name: 'freak2810',
-          email: 'adityamanikanthrao@gmail.com',
-          cart: {
-            items: []
-          }
-        });
-        user.save();
-      }
-    });
     app.listen(3000);
   })
   .catch(err => {
